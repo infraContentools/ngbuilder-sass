63134109eb5b5729ed637f13dba14a5c472fac38
Darlan Alves <me@darlanalv.es>
2014-09-19 09:49:06 -0300

    feat(*): add cssmin. now files are written directly to public path

--- sass.js
+++ sass.js
@@ -7,13 +7,14 @@
  */
 
 module.exports = (function() {
-	var gulp, sass, concat, multipipe, path, _initialized;
+	var cssmin, gulp, sass, concat, multipipe, path, _initialized;
 
 	function init() {
 		if (_initialized) return;
 
 		gulp = require('gulp');
 		sass = require('gulp-sass');
+		cssmin = require('gulp-cssmin');
 		concat = require('gulp-concat');
 		multipipe = require('multipipe');
 		path = require('path');
@@ -22,17 +23,17 @@ module.exports = (function() {
 	}
 
 	function run(context, options, next) {
+		if (options === false) return next();
+
 		if (!context) {
 			return next(new Error('Missing context object'));
 		}
 
 		init();
 
-		var modulePath = context.modulePath,
-			moduleName = context.moduleName;
+		var modulePath = context.modulePath;
 
 		var includePaths = context.sassPaths ? Array.prototype.slice.call(context.sassPaths) : [];
-
 
 		if (typeof includePaths === 'string') {
 			includePaths = [includePaths];
@@ -48,7 +49,8 @@ module.exports = (function() {
 		var pipe = multipipe(
 			gulp.src(path.join(modulePath, 'scss/**/*.scss')),
 			sass(sassOptions),
-			concat('module.css')
+			concat(context.moduleName + '.css'),
+			cssmin()
 		);
 
 		function detach(err) {
@@ -60,7 +62,7 @@ module.exports = (function() {
 		pipe.once('end', detach);
 		pipe.once('finish', detach);
 
-		pipe.pipe(gulp.dest(modulePath));
+		pipe.pipe(gulp.dest(context.public));
 	}
 
 	return {
